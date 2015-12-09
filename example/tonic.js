b1372d09fed1d8fe18ace377a17f8a9fd8467daf
nfrancois <nicolas.francois@influans.com>
2015-12-09 13:26:27 +0100

    Add tonic example

---
+++ example/tonic.js
@@ -0,0 +1,6 @@
+// Sample for tonic : https://tonicdev.com/npm/jsiojs
+var jsiojs = require("jsiojs")
+// Start of my wonderfull project
+jsiojs.createFile("wonderful.js")
+// Oh my god, the file contains a typo. I should fix !
+jsiojs.renameFile("wonderful.js", "wonderfull.js"
