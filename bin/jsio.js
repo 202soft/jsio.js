9eed4e4ffd77864778bafaa8c70b4b623ee048ad
Nicolas François <nicolas.franc@gmail.com>
2016-04-05 18:52:28 +0000

    Refacto generic function to avoid duplicate

--- bin/jsio.js
+++ bin/jsio.js
@@ -1,67 +1,59 @@
 #!/usr/bin/env node
 // bin/jsiojs
 
-var jsiojs = require('../lib/jsiojs_core')
-var colors = require("colors")
+var jsiojs = require('../lib/jsiojs_core');
+var colors = require("colors");
 var program = require('commander');
 
 program.version("42.0.0");
 
-program
-  .command("create [filename]")
-  .description("Create a new js file")
-  .action((filename) => 
+_program("create [filename]", "Create a new js file", (filename) => 
     jsiojs
       .createFile(filename)
-      .then(() => success(`File '${filename}' created with success !`))
-      .fail(fail)    
+      .then(() => _success(`File '${filename}' created with success !`))
+      .fail(_fail)    
   );
 
-program
-  .command("delete [filename]")
-  .description("Delete a js file")
-  .action((filename) => 
+_program("delete [filename]", "Delete a js file", (filename) => 
     jsiojs
       .deleteFile(filename)
-      .then(() => success(`File '${filename}' deleted with success !`))
-      .fail(fail)      
+      .then(() => _success(`File '${filename}' deleted with success !`))
+      .fail(_fail)      
   );
   
-program
-  .command("rename [oldname] [newname]")
-  .description("Rename a js file")
-  .action((oldname, newname) => 
+_program("rename [oldname] [newname]", "Rename a js file", (oldname, newname) => 
       jsiojs
         .renameFile(oldname, newname)
-        .then(() => success(`File '${oldname}' moved to '${newname}' with success !`))
-        .fail(fail)
+        .then(() => _success(`File '${oldname}' moved to '${newname}' with success !`))
+        .fail(_fail)
   );
 
-program
-  .command("copy [source] [destination]")
-  .description("Copy a js file")
-  .action((source, destination) => 
+_program("copy [source] [destination]", "Copy a js file", (source, destination) => 
       jsiojs
         .copyFile(source, destination)
-        .then(() => success(`File '${source}' copied to '${destination}' with success !`))
-        .fail(fail)
+        .then(() => _success(`File '${source}' copied to '${destination}' with success !`))
+        .fail(_fail)
   );
   
-program
-  .command("show [filename]")
-  .description("Show a js file")
-  .action((filename) => 
+_program("show [filename]", "Show a js file", (filename) => 
       jsiojs
         .showFile(filename)
         .then((content) => console.info(content))
-        .fail(fail)
+        .fail(_fail)
   );
 
-program.parse(process.argv)
+program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
 }
 
-function success(message){ console.info(message .green); }
-function fail(message) { console.error(message .red); }
+function _program(command, description, doIt) {
+  program
+    .command(command)
+    .description(description)
+    .action(doIt);
+}
+
+function _success(message){ console.info(message .green); }
+function _fail(message) { console.error(message .red); }
